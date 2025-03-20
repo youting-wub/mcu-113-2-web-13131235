1511d9c7fcd683228c2de9d98d8423a1901cac49
eric <eric560065@gmail.com>
2025-03-20 22:36:02 +0800

    feat(ts): 利用類型定義產品資料

---
+++ src/product.ts
@@ -0,0 +1,18 @@
+export class Product {
+  constructor(id: number, name: string, company: string, price: number, isShow: boolean, createDate: Date, modifyDate: undefined) {
+    this.id = id;
+    this.name = name;
+    this.company = company;
+    this.price = price;
+    this.isShow = isShow;
+    this.createDate = createDate;
+    this.modifyDate = modifyDate;
+  }
+  id: number;
+  name: string;
+  company: string;
+  price: number;
+  isShow: boolean;
+  createDate: Date;
+  modifyDate: undefined | Date;
+}
