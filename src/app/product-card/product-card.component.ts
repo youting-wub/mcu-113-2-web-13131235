6a5464a7a58908d3e352e20fadf5991c55a0e0fe
eric <eric560065@gmail.com>
2025-03-26 18:38:07 +0800

    feat(product): 利用屬性繫結實作圖片位置繫結

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -11,6 +11,7 @@ export class ProductCardComponent {
   author = '作者甲、作者乙、作者丙';
   company = '博碩文化';
   isShow = true;
+  photoUrl = 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img';
   onSetDisplay(isShow: boolean): void {
     this.isShow = isShow;
   }
