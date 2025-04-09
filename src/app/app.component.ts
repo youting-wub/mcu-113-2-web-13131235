a4a6c36fbcd80daa8d0f6096319ff5ffcfd41934
eric <eric560065@gmail.com>
2025-04-09 18:35:26 +0800

    feat(product): 利用 DatePipe 顯示建檔日期

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,7 @@ export class AppComponent {
   company = '碩博文化';
   isShow = true;
   photoUrl = 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img';
+  createDate = new Date('2025/4/9');
   onSetDisplay(isShow: boolean): void {
     this.isShow = isShow;
   }
--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,8 +1,9 @@
+import { DatePipe } from '@angular/common';
 import { booleanAttribute, Component, Input, numberAttribute, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-product-card',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.scss',
 })
@@ -21,6 +22,8 @@ export class ProductCardComponent {
   isShowChange = new EventEmitter<boolean>();
   @Input()
   photoUrl!: string;
+  @Input()
+  createDate!: Date;
   onSetDisplay(isShow: boolean): void {
     this.isShowChange.emit(isShow);
   }
