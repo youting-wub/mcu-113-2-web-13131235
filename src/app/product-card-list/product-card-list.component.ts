e541b09f1bf35dd7b40ce99037066c91dac29e19
eric <eric560065@gmail.com>
2025-04-12 21:26:12 +0800

    feat(product): 依產品清單顯示實作

--- src/app/product-card-list/product-card-list.component.ts
+++ src/app/product-card-list/product-card-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ProductCardComponent } from '../product-card/product-card.component';
 import { Product } from '../models/product';
 
@@ -9,18 +9,5 @@ import { Product } from '../models/product';
   styleUrl: './product-card-list.component.scss',
 })
 export class ProductCardListComponent {
-  product = new Product({
-    id: 1,
-    name: '書籍 A',
-    authors: '作者甲、作者乙、作者丙',
-    company: '碩博文化',
-    isShow: true,
-    photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-    createDate: new Date('2025/4/9'),
-    price: 10000,
-  });
-
-  onSetDisplay(isShow: boolean): void {
-    this.product.isShow = isShow;
-  }
+  @Input() products!: Product[];
 }
--- src/assets/code/product.ts
+++ src/assets/code/product.ts
@@ -1,13 +1,5 @@
 export class Product {
-  constructor(
-    public id: number,
-    public name: string,
-    public company: string,
-    public price: number,
-    public isShow: boolean,
-    public createDate: Date,
-    public modifyDate?: Date
-  ) {}
+  constructor(id: number, name: string, company: string, price: number, isShow: boolean, createDate: Date, modifyDate?: Date) {}
   setDisplay(isShow: boolean): void {
     this.isShow = isShow;
   }
