98fa57be0f8fe16c6de2cb82d9bc1cafeed1dfb8
eric <eric560065@gmail.com>
2025-05-04 14:43:39 +0800

    feat(header): 新增標題元件實作

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,70 +1,65 @@
 import { Product } from './models/product';
 import { Component } from '@angular/core';
 import { ProductCardListComponent } from './product-card-list/product-card-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ProductCardListComponent],
+  imports: [HeaderComponent, ProductCardListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  products: Product[] = [];
-  setEmptyData(): void {
-    this.products = [];
-  }
-  setHasData(): void {
-    this.products = [
-      new Product({
-        id: 1,
-        name: '書籍 A',
-        authors: ['作者甲', '作者乙', '作者丙'],
-        company: '碩博文化',
-        isShow: true,
-        photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-        createDate: new Date('2025/4/9'),
-        price: 10000,
-      }),
-      new Product({
-        id: 2,
-        name: '書籍 B',
-        authors: ['作者甲', '作者乙', '作者丙'],
-        company: '碩博文化',
-        isShow: true,
-        photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-        createDate: new Date('2025/4/9'),
-        price: 10000,
-      }),
-      new Product({
-        id: 3,
-        name: '書籍 C',
-        authors: ['作者甲', '作者乙', '作者丙'],
-        company: '碩博文化',
-        isShow: true,
-        photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-        createDate: new Date('2025/4/9'),
-        price: 10000,
-      }),
-      new Product({
-        id: 4,
-        name: '書籍 D',
-        authors: ['作者甲', '作者乙', '作者丙'],
-        company: '碩博文化',
-        isShow: true,
-        photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-        createDate: new Date('2025/4/9'),
-        price: 10000,
-      }),
-      new Product({
-        id: 5,
-        name: '書籍 E',
-        authors: ['作者甲', '作者乙', '作者丙'],
-        company: '碩博文化',
-        isShow: true,
-        photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-        createDate: new Date('2025/4/9'),
-        price: 10000,
-      }),
-    ];
-  }
+  products: Product[] = [
+    new Product({
+      id: 1,
+      name: '書籍 A',
+      authors: ['作者甲', '作者乙', '作者丙'],
+      company: '碩博文化',
+      isShow: true,
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      createDate: new Date('2025/4/9'),
+      price: 10000,
+    }),
+    new Product({
+      id: 2,
+      name: '書籍 B',
+      authors: ['作者甲', '作者乙', '作者丙'],
+      company: '碩博文化',
+      isShow: true,
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      createDate: new Date('2025/4/9'),
+      price: 10000,
+    }),
+    new Product({
+      id: 3,
+      name: '書籍 C',
+      authors: ['作者甲', '作者乙', '作者丙'],
+      company: '碩博文化',
+      isShow: true,
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      createDate: new Date('2025/4/9'),
+      price: 10000,
+    }),
+    new Product({
+      id: 4,
+      name: '書籍 D',
+      authors: ['作者甲', '作者乙', '作者丙'],
+      company: '碩博文化',
+      isShow: true,
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      createDate: new Date('2025/4/9'),
+      price: 10000,
+    }),
+    new Product({
+      id: 5,
+      name: '書籍 E',
+      authors: ['作者甲', '作者乙', '作者丙'],
+      company: '碩博文化',
+      isShow: true,
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      createDate: new Date('2025/4/9'),
+      price: 10000,
+    }),
+  ];
 }
