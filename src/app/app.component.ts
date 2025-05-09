a10841213578c37990c7145c27345d46418aea61
eric <eric560065@gmail.com>
2025-05-09 13:48:27 +0800

    feat(product-page): 新增產品清單頁面實作

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,65 +1,11 @@
 import { Product } from './models/product';
 import { Component } from '@angular/core';
-import { ProductCardListComponent } from './product-card-list/product-card-list.component';
 import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, ProductCardListComponent],
+  imports: [HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  products: Product[] = [
-    new Product({
-      id: 1,
-      name: '書籍 A',
-      authors: ['作者甲', '作者乙', '作者丙'],
-      company: '碩博文化',
-      isShow: true,
-      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-      createDate: new Date('2025/4/9'),
-      price: 10000,
-    }),
-    new Product({
-      id: 2,
-      name: '書籍 B',
-      authors: ['作者甲', '作者乙', '作者丙'],
-      company: '碩博文化',
-      isShow: true,
-      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-      createDate: new Date('2025/4/9'),
-      price: 10000,
-    }),
-    new Product({
-      id: 3,
-      name: '書籍 C',
-      authors: ['作者甲', '作者乙', '作者丙'],
-      company: '碩博文化',
-      isShow: true,
-      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-      createDate: new Date('2025/4/9'),
-      price: 10000,
-    }),
-    new Product({
-      id: 4,
-      name: '書籍 D',
-      authors: ['作者甲', '作者乙', '作者丙'],
-      company: '碩博文化',
-      isShow: true,
-      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-      createDate: new Date('2025/4/9'),
-      price: 10000,
-    }),
-    new Product({
-      id: 5,
-      name: '書籍 E',
-      authors: ['作者甲', '作者乙', '作者丙'],
-      company: '碩博文化',
-      isShow: true,
-      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-      createDate: new Date('2025/4/9'),
-      price: 10000,
-    }),
-  ];
-}
+export class AppComponent {}
---
+++ src/app/product-page/product-page.component.ts
@@ -0,0 +1,64 @@
+import { Component } from '@angular/core';
+import { Product } from '../models/product';
+import { ProductCardListComponent } from '../product-card-list/product-card-list.component';
+
+@Component({
+  selector: 'app-product-page',
+  imports: [ProductCardListComponent],
+  templateUrl: './product-page.component.html',
+  styleUrl: './product-page.component.scss',
+})
+export class ProductPageComponent {
+  products: Product[] = [
+    new Product({
+      id: 1,
+      name: '書籍 A',
+      authors: ['作者甲', '作者乙', '作者丙'],
+      company: '碩博文化',
+      isShow: true,
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      createDate: new Date('2025/4/9'),
+      price: 10000,
+    }),
+    new Product({
+      id: 2,
+      name: '書籍 B',
+      authors: ['作者甲', '作者乙', '作者丙'],
+      company: '碩博文化',
+      isShow: true,
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      createDate: new Date('2025/4/9'),
+      price: 10000,
+    }),
+    new Product({
+      id: 3,
+      name: '書籍 C',
+      authors: ['作者甲', '作者乙', '作者丙'],
+      company: '碩博文化',
+      isShow: true,
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      createDate: new Date('2025/4/9'),
+      price: 10000,
+    }),
+    new Product({
+      id: 4,
+      name: '書籍 D',
+      authors: ['作者甲', '作者乙', '作者丙'],
+      company: '碩博文化',
+      isShow: true,
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      createDate: new Date('2025/4/9'),
+      price: 10000,
+    }),
+    new Product({
+      id: 5,
+      name: '書籍 E',
+      authors: ['作者甲', '作者乙', '作者丙'],
+      company: '碩博文化',
+      isShow: true,
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      createDate: new Date('2025/4/9'),
+      price: 10000,
+    }),
+  ];
+}
