81727959cf391ffefaae51e7751cd7f9321b5549
eric <eric560065@gmail.com>
2025-05-09 14:19:57 +0800

    feat(router): 切換產品清單與登入頁面實作

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Product } from './models/product';
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'products' },
+  { path: 'products', component: ProductPageComponent },
+  { path: 'login', component: LoginPageComponent },
+];
