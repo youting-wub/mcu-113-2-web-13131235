c22c35a38da4f53edc942f4d024c6e1dd6aac83f
eric <eric560065@gmail.com>
2025-05-18 13:14:33 +0800

    feat(product-detail): 實作依產品編號取得明細資料

--- src/app/product-detail-page/product-detail-page.component.ts
+++ src/app/product-detail-page/product-detail-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, inject } from '@angular/core';
+import { ProductService } from './../services/product.service';
+import { Component, inject, input, numberAttribute, OnInit } from '@angular/core';
 import { Product } from '../models/product';
 import { CurrencyPipe } from '@angular/common';
 import { Router } from '@angular/router';
@@ -9,19 +10,18 @@ import { Router } from '@angular/router';
   templateUrl: './product-detail-page.component.html',
   styleUrl: './product-detail-page.component.scss',
 })
-export class ProductDetailPageComponent {
-  product = new Product({
-    id: 9,
-    name: '書籍 I',
-    authors: ['作者甲', '作者乙', '作者丙'],
-    company: '碩博文化',
-    isShow: true,
-    photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-    createDate: new Date('2025/4/9'),
-    price: 10000,
-  });
+export class ProductDetailPageComponent implements OnInit {
+  id = input.required<number, string | number>({ transform: numberAttribute });
+
+  product!: Product;
 
   readonly router = inject(Router);
+
+  private ProductService = inject(ProductService);
+
+  ngOnInit(): void {
+    this.product = this.ProductService.getById(this.id());
+  }
   onEdit(): void {
     this.router.navigate(['product', 'form', this.product.id]);
   }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -108,6 +108,11 @@ export class ProductService {
       price: 10000,
     }),
   ];
+
+  getById(productId: number): Product {
+    return this._data.find(({ id }) => id === productId)!;
+  }
+
   getList(name: string | undefined, index: number, size: number): { data: Product[]; count: number } {
     const startIndex = (index - 1) * size;
     const endIndex = startIndex + size;
@@ -116,7 +121,7 @@ export class ProductService {
   }
 
   add(product: Readonly<Product>): void {
-    const id = this._data.length === 0 ? 1 : Math.max(...this._data.map(({ id }) => id));
+    const id = this._data.length === 0 ? 1 : Math.max(...this._data.map(({ id }) => id)) + 1;
     this._data.push(new Product({ ...product, id }));
   }
   remove(productId: number): void {
